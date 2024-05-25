723eb2babc0f00d6c2db98075c3bda0d192ca28d
AlyssaUrsua <alyssa_021913@binalatongan.edu.ph>
2024-05-25 15:35:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function clickLike1(){
    let totalLikes1 = parseInt(countLikes1.value) + 1
    countLikes1.textContent = totalLikes1.toString()
 }
-btnlike1.addEventListener("click",clickLike1) 
+btnlike1.addEventListener("click",clickLikes1) 
+
+
+ function clickLikes2(){
+    let totalLikes2 = parseInt(countLikes2.value) + 1
+    countLikes2.textContent = totalLikes2.toString()
+ }

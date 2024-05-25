031863070c6da85c30298dd10f89d3873dced807
AlyssaUrsua <alyssa_021913@binalatongan.edu.ph>
2024-05-25 16:01:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const btnlike1 = document.getElementIdBy("btnlike1")
 const countLikes1 = document.getElementById("countLikes1")
-
+const btnlike2 = document.getElementIdBy("btnlike2")
+const countLikes2 = document.getElementById("countLikes2")
 function clickLike1(){
    let totalLikes1 = parseInt(countLikes1.value) + 1
    countLikes1.textContent = totalLikes1.toString()

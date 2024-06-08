38b3352ec53fd9fe489d231436ffd76b607ba6e2
AlyssaUrsua <alyssa_021913@binalatongan.edu.ph>
2024-06-08 15:20:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function clickLike2() {
   countLikes2.textContent = totalLikes2.toString()
 }
 function submitComment(){
-commentbox.textContent += comment.value.toString() + "\n"
+commentbox.textContent = comment.value.toString() + "\n"
 comment.value=""
   }
 btnLike1.addEventListener("click",clickLike1)
